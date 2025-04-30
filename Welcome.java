ab8b2dc1841f40174cebf196d1e2d73d971753bd
mary kenny <marykenny2704@yahoo.com>
2025-04-30 15:05:20 +0200

    ex 1

--- Welcome.java
+++ Welcome.java
@@ -8,7 +8,5 @@
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome FemCoders to Java World!");
-        System.out.println("test");
     }
-
 }
