fb945cad9baa564dce168e5f583262672150f155
mary kenny <marykenny2704@yahoo.com>
2025-04-30 15:37:20 +0200

    ex 1 (previously didn't commit + push correctly

--- EnergyEfficiency.java
+++ EnergyEfficiency.java
@@ -2,25 +2,35 @@ import java.sql.SQLOutput;
 
 public class EnergyEfficiency {
     public static void main(String[] args) {
-        //inicializar variables (del tipo que veas conveniente) para las calificaciones A, B, C, D, E, F y G que correspondan a eficiencia energética e imprime en terminal concatenando cada string con cada variable creada. 
+        //inicializar variables (del tipo que veas conveniente) para las calificaciones A, B, C, D, E, F y G
+        // que correspondan a eficiencia energética e imprime en terminal concatenando cada string con cada
+        // variable creada.
+
+        char veryGood = 'A';
+        char good = 'B';
+        char fairlyGood = 'C';
+        char neutral = 'D';
+        char fairlyBad = 'E';
+        char bad = 'F';
+        char veryBad = 'G';
 
         //Reemplaza <very_good> concatenando el String con la variable creada. 
-        System.out.println("Una calificación <very_good> es muy buena.");
+        System.out.println("Una calificación  " + veryGood + " es muy buena.");
 
         //Reemplaza <good> concatenando el String con la variable creada. 
-        System.out.println("Una calificación <good> es buena.");
+        System.out.println("Una calificación " + good + " es buena.");
         
         //Reemplaza <fairly_good> concatenando el String con la variable creada. 
-        System.out.println("Una calificación <fairly_good> es medianamente buena.");
+        System.out.println("Una calificación " + fairlyGood + " es medianamente buena.");
         
         //Reemplaza <neutral> concatenando el String con la variable creada. 
-        System.out.println("Una calificación <neutral> es neutral.");
+        System.out.println("Una calificación " + neutral + " es neutral.");
         
         //Reemplaza <fairly_bad> concatenando el String con la variable creada. 
-        System.out.println("Una calificación <fairly_bad> es medianamente mala.");
+        System.out.println("Una calificación " + fairlyBad + " es medianamente mala.");
         
         //Reemplaza <bad> concatenando el String con la variable creada. 
-        System.out.println("Una calificación <bad> es mala.");
+        System.out.println("Una calificación " + bad + " es mala.");
 
         //Reemplaza <very_bad> concatenando el String con la variable creada.
         System.out.println("Una calificación <very_bad> es muy mala.");
