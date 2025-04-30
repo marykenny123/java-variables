c1c38844eaadc143d5cdd04ee6ff57297d4f5024
mary kenny <marykenny2704@yahoo.com>
2025-04-30 15:04:19 +0200

    exercise 1

--- Welcome.java
+++ Welcome.java
@@ -8,6 +8,7 @@
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome FemCoders to Java World!");
+        System.out.println("test");
     }
 
 }
